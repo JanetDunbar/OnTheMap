ddb67bff9cd08ec04018cc6ae60dd597098ff819
jmd <jmd@amberlight.com>
2015-06-02 17:44:47 -0500

    Added funcs login and getStudentLocations.  Added IBOutlets for loginButton and debugLabel.

--- OnTheMap/LoginViewController.swift
+++ OnTheMap/LoginViewController.swift
@@ -10,21 +10,30 @@ import UIKit
 import Foundation
 
 
-class LoginViewController: UIViewController {
+class LoginViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var username: UITextField!
     @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var loginButton: BorderedButton!
+    @IBOutlet weak var debugLabel: UILabel!
     
     @IBAction func login(sender: AnyObject) {
         
         // Create dictionary of userNames and passwords
         var udacity = ["username": "", "password": ""]
+        udacity["username"] = username.text
+        //println("username = /(username.text)")
+        udacity["password"] = password.text
         
         let request = NSMutableURLRequest(URL: NSURL(string: "https://www.udacity.com/api/session")!)
         request.HTTPMethod = "POST"
         request.addValue("application/json", forHTTPHeaderField: "Accept")
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-        request.HTTPBody = "{\"udacity\": {\"username\": \"account@domain.com\", \"password\": \"********\"}}".dataUsingEncoding(NSUTF8StringEncoding)
+//        request.HTTPBody = "{\"udacity\": {\"username\": \"account@domain.com\", \"password\": \"********\"}}".dataUsingEncoding(NSUTF8StringEncoding)
+        // json = { udacity : { username: "foo@bar.com", password: "1234" } }
+        let un = username.text
+        let pw = password.text
+        request.HTTPBody = "{\"udacity\": {\"username\": \"\(un)\", \"password\": \"\(pw)\"}}".dataUsingEncoding(NSUTF8StringEncoding)
         let session = NSURLSession.sharedSession()
         let task = session.dataTaskWithRequest(request) { data, response, error in
             if error != nil { // Handle error…
@@ -32,18 +41,48 @@ class LoginViewController: UIViewController {
             }
             let newData = data.subdataWithRange(NSMakeRange(5, data.length - 5)) /* subset response data! */
             println(NSString(data: newData, encoding: NSUTF8StringEncoding))
+            /*
+            Optional({"account": {"registered": true, "key": "280195754"}, "session": {"id": "1464816385Sb5f1302a6d754bd54a7f63d5bb85bc9b", "expiration": "2015-08-01T21:26:25.946330Z"}})
+            */
+            //TODO: ?Convert NSString optional to JSON.  Then convert JSON to swift object?
         }
         task.resume()
+        getStudentLocations()
     }
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        username.delegate = self
+        password.delegate = self
+    }
+    
+    // After typing in a textField, user presses return to end input for that textField.
+    func textFieldShouldReturn(textField:UITextField)->Bool{
+        
+        textField.resignFirstResponder()
+        return true
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    // Get student locations, Parse API
+    func getStudentLocations(limit:Int = 100){
+        
+        let request = NSMutableURLRequest(URL: NSURL(string: "https://api.parse.com/1/classes/StudentLocation")!)
+        request.addValue("QrX47CA9cyuGewLdsL7o5Eb8iug6Em8ye0dnAbIr", forHTTPHeaderField: "X-Parse-Application-Id")
+        request.addValue("QuWThTdiRmTux3YaDseUSEpUKo7aBYM737yKd4gY", forHTTPHeaderField: "X-Parse-REST-API-Key")
+        let session = NSURLSession.sharedSession()
+        let task = session.dataTaskWithRequest(request) { data, response, error in
+            if error != nil { // Handle error...
+                return
+            }
+            println(NSString(data: data, encoding: NSUTF8StringEncoding))
+        }
+        task.resume()
     }
 
     /*

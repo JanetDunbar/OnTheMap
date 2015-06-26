d3f2b49147ec0011db048a2085d181883a023255
jmd <jmd@amberlight.com>
2015-06-26 08:58:18 -0500

    Client's loginWithClient function works, logs in properly, but not downloading student  data.

--- OnTheMap/LoginViewController.swift
+++ OnTheMap/LoginViewController.swift
@@ -25,32 +25,11 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
         udacity["username"] = username.text
         //println("username = /(username.text)")
         udacity["password"] = password.text
-        
-        let request = NSMutableURLRequest(URL: NSURL(string: "https://www.udacity.com/api/session")!)
-        request.HTTPMethod = "POST"
-        request.addValue("application/json", forHTTPHeaderField: "Accept")
-        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-//        request.HTTPBody = "{\"udacity\": {\"username\": \"account@domain.com\", \"password\": \"********\"}}".dataUsingEncoding(NSUTF8StringEncoding)
-        // json = { udacity : { username: "foo@bar.com", password: "1234" } }
         let un = username.text
-        let pw = password.text
-        request.HTTPBody = "{\"udacity\": {\"username\": \"\(un)\", \"password\": \"\(pw)\"}}".dataUsingEncoding(NSUTF8StringEncoding)
-        println("LoginVC request.HTTPBody = \(request.HTTPBody)")
-        let session = NSURLSession.sharedSession()
-        let task = session.dataTaskWithRequest(request) { data, response, error in
-            if error != nil { // Handle error…
-                return
-            }
-            let newData = data.subdataWithRange(NSMakeRange(5, data.length - 5)) /* subset response data! */
-            let loginString = NSString(data: newData, encoding: NSUTF8StringEncoding) as! String
-            println("loginString = \(loginString)")
-            /*
-            Optional({"account": {"registered": true, "key": "280195754"}, "session": {"id": "1464816385Sb5f1302a6d754bd54a7f63d5bb85bc9b", "expiration": "2015-08-01T21:26:25.946330Z"}})
-            */
-            //TODO: ?Convert NSString optional to JSON.  Then convert JSON to swift object?
-        }
-        task.resume()
-        getStudentLocations()
+        let pwd = password.text
+        let client = Client()
+        client.loginWithClient(un, pw: pwd)
+       
     }
     
     override func viewDidLoad() {

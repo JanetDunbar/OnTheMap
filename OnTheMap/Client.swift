ce4883a8098493f5e823d67705988c53534b8165
jmd <jmd@amberlight.com>
2015-07-01 14:31:11 -0500

    Changed limit from 3 to 100 in getStudentLocations.

--- OnTheMap/Client.swift
+++ OnTheMap/Client.swift
@@ -39,19 +39,17 @@ class Client {
         }
         task.resume()
         
-        // TODO:!!!! Is now in login func of LoginViewController-move?????
-        //getStudentLocations()
     }
     
-    // Get student locations, via Parse API.  TODO: Change!!!!Using small number for testing
+    // Get student locations, via Parse API.  TODO: Change!!!!Using small number for testing.  Need alert?
     func getStudentLocations(completion: ()->()){
         
-        let request = NSMutableURLRequest(URL: NSURL(string: "https://api.parse.com/1/classes/StudentLocation?limit=3")!)
+        let request = NSMutableURLRequest(URL: NSURL(string: "https://api.parse.com/1/classes/StudentLocation?limit=100")!)
         request.addValue("QrX47CA9cyuGewLdsL7o5Eb8iug6Em8ye0dnAbIr", forHTTPHeaderField: "X-Parse-Application-Id")
         request.addValue("QuWThTdiRmTux3YaDseUSEpUKo7aBYM737yKd4gY", forHTTPHeaderField: "X-Parse-REST-API-Key")
         let session = NSURLSession.sharedSession()
         let task = session.dataTaskWithRequest(request) { data, response, error in
-            if error != nil { // Handle error...
+            if error != nil { // !!!!!!!!Handle error...
                 return
             }
             

99027af03609d235da5cb163751d104faf514b36
jmd <jmd@amberlight.com>
2015-07-19 20:44:19 -0500

    Added logout button and functionality to TableViewController.

--- OnTheMap/TableViewController.swift
+++ OnTheMap/TableViewController.swift
@@ -123,4 +123,29 @@ class TableViewController: UITableViewController, UITableViewDataSource, UITable
     }
     
     
+    @IBAction func logout(sender: UIBarButtonItem) {
+        
+        let request = NSMutableURLRequest(URL: NSURL(string: "https://www.udacity.com/api/session")!)
+        request.HTTPMethod = "DELETE"
+        var xsrfCookie: NSHTTPCookie? = nil
+        let sharedCookieStorage = NSHTTPCookieStorage.sharedHTTPCookieStorage()
+        for cookie in sharedCookieStorage.cookies as! [NSHTTPCookie] {
+            if cookie.name == "XSRF-TOKEN" { xsrfCookie = cookie }
+        }
+        if let xsrfCookie = xsrfCookie {
+            request.addValue(xsrfCookie.value!, forHTTPHeaderField: "X-XSRF-Token")
+        }
+        let session = NSURLSession.sharedSession()
+        let task = session.dataTaskWithRequest(request) { data, response, error in
+            if error != nil { // Handle error…
+                //
+                println(error)
+                return
+            }
+            let newData = data.subdataWithRange(NSMakeRange(5, data.length - 5)) /* subset response data! */
+            println(NSString(data: newData, encoding: NSUTF8StringEncoding))
+            self.dismissViewControllerAnimated(true, completion: nil)
+        }
+        task.resume()
+    }
 }

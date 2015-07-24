18f95baa6927f19d8fbe5fbea2097a45fb43c891
jmd <jmd@amberlight.com>
2015-07-24 17:22:28 -0500

    Added CollectionViewController to storyboard, embedded in navigation controller, related it to MapTabBarViewController, made custom cell, CollectionViewCell, and added two labels to it:  nameLabel and urlLabel.

---
+++ OnTheMap/CollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.swift
+//  OnTheMap
+//
+//  Created by Dr. Janet M. Dunbar on 7/24/15.
+//  Copyright (c) 2015 Dr. Janet M. Dunbar. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+}
